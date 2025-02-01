19cf4480775350e9c2a69a6d7798647c84c4efe7
Danys-ai <nkongnitchindadany@gmail.com>
2025-02-02 00:17:58 +0100

    Add files via upload

---
+++ registration.py
@@ -0,0 +1,8 @@
+from gym.envs.registration import register
+
+register(
+    id="TreasureHuntEnv-v0",  # Unique identifier for your environment
+    entry_point="aufgabe 2:TreasureHuntEnv-v0",  # Python file name (module) and class name
+
+)
+
